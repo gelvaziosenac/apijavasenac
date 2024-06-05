c66d901d6817052dd1111e7f85546a013f122168
Gelvazio <gelvazio@hotmail.com>
2024-06-04 22:08:03 -0300

    ok

--- src/main/java/bancocrudspringboot/controller/ProdutoController.java
+++ src/main/java/bancocrudspringboot/controller/ProdutoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import bancocrudspringboot.exception.ResourceNotFoundException;
 import bancocrudspringboot.model.ConsultaPadrao;
+import bancocrudspringboot.model.OperadoresConsulta;
 import bancocrudspringboot.model.Produto;
 import bancocrudspringboot.repository.ProdutoRepository;
 
@@ -41,19 +42,87 @@ public class ProdutoController {
 			return this.produtoRepository.findAll();
 		}
 
-		switch (campoConsulta) {
-			case "codigoConsulta":
-				Produto produto = produtoRepository.findById(Long.parseLong(cadastro.getValor1()))
-						.orElseThrow(() -> new ResourceNotFoundException("Registro não encontrado para o ID :: " + cadastro.getValor1()));
+		// OPERADOR -> IGUAL
+		String operador = cadastro.getOperador();
 
-				listaProduto.add(produto);
-				break;
-			case "descricaoConsulta":
-                listaProduto = this.produtoRepository.findProdutoByDescricao(cadastro.getValor1());
-				break;
-			default:
-				new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());
-				break;
+		if(operador.equals(OperadoresConsulta.OPERADOR_IGUAL)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+					Produto produto = produtoRepository.findById(Long.parseLong(cadastro.getValor1()))
+							.orElseThrow(() -> new ResourceNotFoundException("Registro não encontrado para o ID: " + cadastro.getValor1()));
+					listaProduto.add(produto);
+					break;
+				case "descricaoConsulta":
+					listaProduto = this.produtoRepository.findProdutoByDescricao(cadastro.getValor1());
+					break;
+				case "estoqueConsulta":
+					listaProduto = this.produtoRepository.findProdutoByEstoque(Integer.parseInt(cadastro.getValor1()));
+					break;
+				case "precoConsulta":
+					listaProduto = this.produtoRepository.findProdutoByPreco(Double.parseDouble(cadastro.getValor1()));
+					break;					
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else if(operador.equals(OperadoresConsulta.OPERADOR_MAIOR_OU_IGUAL)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+				    listaProduto = this.produtoRepository.findProdutoMaiorIgual(Long.parseLong(cadastro.getValor1()));
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else if(operador.equals(OperadoresConsulta.OPERADOR_MENOR_IGUAL)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+				    listaProduto = this.produtoRepository.findProdutoMenorIgual(Long.parseLong(cadastro.getValor1()));
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else if(operador.equals(OperadoresConsulta.OPERADOR_MENOR_QUE)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+				    listaProduto = this.produtoRepository.findProdutoMenorQue(Long.parseLong(cadastro.getValor1()));
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		}  else if(operador.equals(OperadoresConsulta.OPERADOR_MAIOR_QUE)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+				    listaProduto = this.produtoRepository.findProdutoMaiorQue(Long.parseLong(cadastro.getValor1()));
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else if(operador.equals(OperadoresConsulta.OPERADOR_DIFERENTE_DE)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+				    listaProduto = this.produtoRepository.findProdutoDiferenteDe(Long.parseLong(cadastro.getValor1()));
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else if(operador.equals(OperadoresConsulta.OPERADOR_ENTRE)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+				    listaProduto = this.produtoRepository.findProdutoEntreValores(Long.parseLong(cadastro.getValor1()), Long.parseLong(cadastro.getValor2()));
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else if(operador.equals(OperadoresConsulta.OPERADOR_CONTIDO_EM)){
+			switch (campoConsulta) {
+				case "codigoConsulta":
+					String[] names = {cadastro.getValor1()};
+				    listaProduto = this.produtoRepository.findProdutoContidoEm(names);
+					break;
+				default:
+					throw new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo());				
+			}
+		} else {
+			throw new ResourceNotFoundException("Operador não desenvolvido!" + cadastro.getOperador());				
 		}
 
 		return listaProduto;
--- src/main/java/bancocrudspringboot/repository/ProdutoRepository.java
+++ src/main/java/bancocrudspringboot/repository/ProdutoRepository.java
@@ -14,4 +14,30 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
     @Query(value = "select * from produto where descricao ilike concat('%', :descricao, '%')", nativeQuery = true)
     List<Produto>findProdutoByDescricao(@Param("descricao")String descricao);
+    
+    List<Produto>findProdutoByEstoque(int estoque);
+    
+    List<Produto>findProdutoByPreco(double preco);
+
+    @Query(value = "select * from produto where id >= :codigo", nativeQuery = true)
+    List<Produto> findProdutoMaiorIgual(@Param("codigo")long codigo);
+    
+    @Query(value = "select * from produto where id <= :codigo", nativeQuery = true)
+    List<Produto> findProdutoMenorIgual(@Param("codigo")long codigo);
+
+    @Query(value = "select * from produto where id < :codigo", nativeQuery = true)
+    List<Produto> findProdutoMenorQue(@Param("codigo")long codigo);
+    
+    @Query(value = "select * from produto where id > :codigo", nativeQuery = true)
+    List<Produto> findProdutoMaiorQue(@Param("codigo")long codigo);
+    
+    @Query(value = "select * from produto where id <> :codigo", nativeQuery = true)
+    List<Produto> findProdutoDiferenteDe(@Param("codigo")long codigo);
+    
+    @Query(value = "select * from produto where id between :codigo1 and :codigo2", nativeQuery = true)
+    List<Produto> findProdutoEntreValores(@Param("codigo1")long codigo1, @Param("codigo2")long codigo2);
+    
+    @Query(value = "select * from produto where id in :listaCodigo", nativeQuery = true)
+    List<Produto> findProdutoContidoEm(@Param("listaCodigo")String[] listaCodigo);
+    
 }
